e0db4521434d6c0e8b419dc1a6c118b8c9d3cd74
gideonmandu <gideonmandu@gmail.com>
2021-12-07 11:50:25 +0300

    added views and configuring them in controller

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,3 +1,6 @@
 <footer>
     <h1>This is a footer</h1>
 </footer>
+</body>
+
+</html>
--- app/Views/myview.php
+++ app/Views/myview.php
@@ -1,16 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to CI4</title>
-</head>
-
-<body>
     <h1>Welcome to CI4</h1>
     <h1>Welcome to Code Igniter 4</h1>
-</body>
-
-</html>
