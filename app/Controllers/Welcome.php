cd18ab6936488e0e9900bb46f17f1d7a55e24244
gideonmandu <gideonmandu@gmail.com>
2021-12-07 11:37:40 +0300

    remaping URIs

--- app/Controllers/Welcome.php
+++ app/Controllers/Welcome.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Welcome extends Controller
 {
@@ -14,5 +15,23 @@ class Welcome extends Controller
     public function test($name)
     {
         echo "Welcome to " . $name;
+    }
+
+    public function address($city, $country)
+    {
+        echo "You are from " . $city . " and " . $country;
+    }
+
+    public function _remap($method, $param1 = null, $param2 = null)
+    // class method to remap if method in uri does not exist.
+    {
+        if (method_exists($this, $method)) {
+            return $this->$method($param1, $param2);
+        }
+        // else {
+        //     $this->index();
+        // }
+        // throw exception if ur not found
+        throw PageNotFoundException::forPageNotFound();
     }
 }
